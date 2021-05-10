4d42fea4d654e6ea67514e10bafb7ccfb7feb36e
msvolt123 <msvolt123@gmail.com>
2021-05-10 11:44:32 +0300

    3.4. Заголовок в файле java [#159036]

---
+++ src/ru/job4j/Variables.java
@@ -0,0 +1,23 @@
+package ru.job4j;
+
+public class Variables {
+    public static void main(String[] args) {
+        int age = 31;
+        System.out.println(age);
+        char gender = 'M';
+        System.out.println(gender);
+        float height = 168.7F;
+        System.out.println(height);
+        short bricks = 791;
+        System.out.println(bricks);
+        long bacterium = 56987413896477852L;
+        System.out.println(bacterium);
+        double space = 147.0989729121;
+        System.out.println(space);
+        boolean job = true;
+        System.out.println(job);
+        double size = 109.4;
+        System.out.println(size);
+
+    }
+}
