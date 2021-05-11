15cc3ec4534cee657aab62a5884e5a0045a52356
msvolt123 <msvolt123@gmail.com>
2021-05-11 19:54:22 +0300

    5. Конвертер валюты [#41583]

--- src/ru/job4j/ru/job4j/converter/Converter.java
+++ src/ru/job4j/ru/job4j/converter/Converter.java
@@ -1,5 +1,6 @@
 package ru.job4j.ru.job4j.converter;
 
+import javax.annotation.processing.SupportedSourceVersion;
 import javax.swing.*;
 
 public class Converter {
@@ -13,11 +14,24 @@ public class Converter {
         int rsl = value / 60;
         return rsl;
     }
+    public static int dollarToRuble (int value) {
+        int rsl = 60 * value;
+        return rsl;
+    }
+    public static int euroToRuble (int value) {
+        int rsl = 70 * value;
+        return rsl;
+    }
 
     public static void main(String[] args) {
         int euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
         int dollar = Converter.rubleToDollar(200);
-        System.out.println("200 ruble are " + dollar + " dollar.");
+        System.out.println("200 rubles are " + dollar + " dollar.");
+        int ruble1 = Converter.dollarToRuble(12);
+        System.out.println("12 dollars are " + ruble1 + " rubles.");
+        int ruble2 = Converter.euroToRuble(7);
+        System.out.println("7 euro are " + ruble2 + " rubles.");
+
     }
 }
