ce36a7c977a6acc91b2f7552157d59dc003ca7bf
msvolt123 <msvolt123@gmail.com>
2021-05-11 19:40:25 +0300

    5. Конвертер валюты [#41583]

---
+++ src/ru/job4j/ru/job4j/calculator/MathFunc.java
@@ -0,0 +1,16 @@
+package ru.job4j.ru.job4j.calculator;
+
+public class MathFunc {
+
+    public static int func1(int x) {
+        int y = x * x + 1;
+        return y;
+    }
+
+      public static void main(String[] args) {
+        int result1 = MathFunc.func1(100);
+        int result = result1;
+        System.out.println(result);
+    }
+}
+
---
+++ src/ru/job4j/ru/job4j/converter/Converter.java
@@ -0,0 +1,23 @@
+package ru.job4j.ru.job4j.converter;
+
+import javax.swing.*;
+
+public class Converter {
+
+    public static int rubleToEuro (int value) {
+        int rsl = value / 70;
+        return rsl;
+    }
+
+    public static int rubleToDollar (int value) {
+        int rsl = value / 60;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        int euro = Converter.rubleToEuro(140);
+        System.out.println("140 rubles are " + euro + " euro.");
+        int dollar = Converter.rubleToDollar(200);
+        System.out.println("200 ruble are " + dollar + " dollar.");
+    }
+}
