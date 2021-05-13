cad20b5b2fabc5906427655589fa73a71894d3e0
msvolt123 <msvolt123@gmail.com>
2021-05-13 21:44:06 +0300

    9.1. String [#237407]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -5,8 +5,8 @@ public class SqArea {
     public static double square(int p, double k) {
         double h = p / (2 * (k + 1));
         double l = (h * k);
-        double rslS = (l * h);
-        return rslS;
+        return l * h;
+
     }
     public static void main(String[] args) {
         double result1 = SqArea.square(6,2);
