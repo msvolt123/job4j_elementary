5f0090014b32177fc7b91b5ac0b8b3e831015d0a
msvolt123 <msvolt123@gmail.com>
2021-05-13 21:21:14 +0300

    9.1. String [#237407]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,10 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!" + " But i am newbie. ";
+        int year = 2021;
+        System.out.println(idea + year);
+
+    }
+}
