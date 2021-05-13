ee24ce79b226a37f54c7326c9b9911838c74068a
msvolt123 <msvolt123@gmail.com>
2021-05-13 22:04:08 +0300

    9. Площадь треугольника. [#175474 #1281]

---
+++ src/ru/job4j/calculator/Out.java
@@ -0,0 +1,11 @@
+package ru.job4j.calculator;
+
+public class Out {
+    public static void main(String [] args) {
+        String name = "Oleg";
+        int age = 33;
+        String separate = " - ";
+        String id = name + separate + age + " years old";
+        System.out.println(id);
+    }
+}
