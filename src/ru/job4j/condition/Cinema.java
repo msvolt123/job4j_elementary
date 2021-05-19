5940e908e218dbe9c4a555ccc5f1defb1c694bae
msvolt123 <msvolt123@gmail.com>
2021-05-19 18:56:46 +0300

    Оператор &&

--- src/ru/job4j/condition/Cinema.java
+++ src/ru/job4j/condition/Cinema.java
@@ -1,18 +1,16 @@
 package ru.job4j.condition;
 
 public class Cinema {
-    public static void access(int age) {
-        System.out.println("The age of the customer is : " + age);
-        if (age > 18) {
-            System.out.println("Welcome to the cinema.");
-        } else {
-            System.out.println("It is not for you.");
-        }
-
+    public static void permission(boolean allowByParent, boolean hasMoney) {
+        boolean permission = allowByParent && hasMoney;
+        System.out.println(permission ? "I can go to the cinema." : "I can't.");
     }
 
     public static void main(String[] args) {
-        Cinema.access(21);
-        Cinema.access(16);
+        Cinema.permission(true, true);
+        Cinema.permission(true, false);
+        Cinema.permission(false, true);
+        Cinema.permission(false, false);
+
     }
 }
