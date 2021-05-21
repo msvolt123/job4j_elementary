b9b42941190503ae261c23be679fbeee568738ee
msvolt123 <msvolt123@gmail.com>
2021-05-21 17:49:34 +0300

    4.3.6. Math.abs

--- src/ru/job4j/condition/ChessBoard.java
+++ src/ru/job4j/condition/ChessBoard.java
@@ -4,11 +4,11 @@ import com.sun.source.tree.BreakTree;
 
 public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
-        return Math.abs(x1 - x2) == Math.abs(y1 - y2) ? Math.abs(y2 - y1) : 0;
+        return (x1 >= 0 && x1 <= 7) && (y1 >= 0 && y2 <= 7) && Math.abs(x1 - x2) == Math.abs(y1 - y2) ? Math.abs(y2 - y1) : 0;
 
     }
 
     public static void main(String[] args) {
-        System.out.println(ChessBoard.way(2, 6, -1, 1));
+        System.out.println(ChessBoard.way(2, 6, 4, 10));
     }
 }
