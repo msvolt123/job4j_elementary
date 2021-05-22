2337f8f153f016159564c936ef4dd254eb20f93c
msvolt123 <msvolt123@gmail.com>
2021-05-22 12:12:05 +0300

    Отладка программы в IDEA

---
+++ src/ru/job4j/condition/ThreeMax.java
@@ -0,0 +1,20 @@
+package ru.job4j.condition;
+
+import javax.print.attribute.standard.MediaSize;
+
+public class ThreeMax {
+    public static int max(int first, int second, int third) {
+        int result = first;
+        if (first >= second && first >= third) {
+            result = first;
+        }
+        if (second >= first && second >= third) {
+            result = second;
+        }
+        if (third >= second && third >= first) {
+            result = third;
+        }
+        return result;
+    }
+}
+
