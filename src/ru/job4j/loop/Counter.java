d2dc3313c630758d1df9680cbd071c135aca7bfd
msvolt123 <msvolt123@gmail.com>
2021-05-23 12:26:30 +0300

    Сумма чисел.

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -11,5 +11,7 @@ public class Counter {
 
     public static void main(String[] args) {
         System.out.println(sum(3, 8));
+        System.out.println(sum(3, 8));
+        System.out.println(sum(1, 1));
     }
 }
