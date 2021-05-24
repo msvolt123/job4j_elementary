b08b05074dad5b3791303f59b6b1c7e2cefeaa9e
msvolt123 <msvolt123@gmail.com>
2021-05-24 20:16:17 +0300

    Простое число

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -4,8 +4,8 @@ public class CheckPrimeNumber {
     public static boolean check(int number) {
         boolean prime;
         prime = number > 1;
-        for (int i = 2; i <= number; i++) {
-            if (number % 2 == 0) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
                 prime = false;
                 break;
             }
