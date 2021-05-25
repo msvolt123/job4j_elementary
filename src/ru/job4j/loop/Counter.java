3b3f9ac3e707ffc3797608701af35a2663970150
msvolt123 <msvolt123@gmail.com>
2021-05-25 20:35:37 +0300

    Именование классов и переменных

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -12,9 +12,12 @@ public class Counter {
     public static int sumByEven(int start, int finish) {
         int sum = 0;
         for (int i = start; i <= finish; i++) {
-            if (i % 2 == 0)
+            if (i % 2 == 0) {
                 sum += i;
+            }
+
         }
+
         return sum;
     }
 }
