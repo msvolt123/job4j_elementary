4dea239ba9bbd106c9207912b4889b8e9d1630b9
msvolt123 <msvolt123@gmail.com>
2021-05-27 19:19:15 +0300

    Размер массива

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -2,8 +2,13 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
+        int[] ints = new int[10];
         short[] ages = new short[10];
         String[] surname = new String[100500];
         float[] prices = new float[40];
+        System.out.println("Размер массива ints равен: " + ints.length);
+        System.out.println("Размер массива ages равен: " + ages.length);
+        System.out.println("Размер массива surname равен: " + surname.length);
+        System.out.println("Размер массива prices равен: " + prices.length);
     }
 }
