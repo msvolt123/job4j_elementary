82fdd59242aea33d29d199e27db945b5bd42a154
msvolt123 <msvolt123@gmail.com>
2021-05-27 19:28:52 +0300

    Заполнение массива

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -2,13 +2,14 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        int[] ints = new int[10];
-        short[] ages = new short[10];
-        String[] surname = new String[100500];
-        float[] prices = new float[40];
-        System.out.println("Размер массива ints равен: " + ints.length);
-        System.out.println("Размер массива ages равен: " + ages.length);
-        System.out.println("Размер массива surname равен: " + surname.length);
-        System.out.println("Размер массива prices равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Fedor Petrov";
+        names[1] = "Ivan Naumov";
+        names[2] = "Konstantin Deev";
+        names[3] = "Ivanov Ivan";
+        System.out.println(names[2]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
