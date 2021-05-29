f6a76becad2310951ec3a8cb01a25f1f4d5a8fbb
msvolt123 <msvolt123@gmail.com>
2021-05-29 12:56:49 +0300

    Классический поиск перебором

---
+++ test/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FindLoopTest {
+
+    @Test
+    public void whenArrayHas5Then0() {
+        int[] data = {5, 4, 3, 2};
+        int el = 5;
+        int result = FindLoop.indexOf(data, el);
+        int expected = 0;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenArrayHas2Then3() {
+        int[] data = {5, 4, 3, 2};
+        int el = 2;
+        int result = FindLoop.indexOf(data, el);
+        int expected = 3;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenArrayHas7ThenRst() {
+        int[] data = {5, 4, 3, 2};
+        int el = 7;
+        int result = FindLoop.indexOf(data, el);
+        int expected = -1;
+        Assert.assertEquals(expected, result);
+    }
+}
