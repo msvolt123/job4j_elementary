cf5c49f07feb2be133e734b69d3d96eaa6bbc1cd
msvolt123 <msvolt123@gmail.com>
2021-06-01 18:42:17 +0300

    Поиск индекса в диапазоне.

--- src/ru/job4j/array/EndsWith.java
+++ src/ru/job4j/array/EndsWith.java
@@ -4,7 +4,7 @@ public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
         for (int i = 0; i < post.length; i++) {
-            if (word[word.length - 1 - i] != post[post.length - 1 - i]) {
+            if (word[word.length - i - 1] != post[post.length - i - 1]) {
                 result = false;
                 break;
             }
