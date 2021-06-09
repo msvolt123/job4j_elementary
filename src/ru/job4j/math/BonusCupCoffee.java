3d1bc0a856ca73ec71c5f26d42bedccd2b816c23
msvolt123 <msvolt123@gmail.com>
2021-06-09 20:28:24 +0300

    1

--- src/ru/job4j/math/BonusCupCoffee.java
+++ src/ru/job4j/math/BonusCupCoffee.java
@@ -2,13 +2,13 @@ package ru.job4j.math;
 
 public class BonusCupCoffee {
     public static int countCup(int count, int n) {
-        if (count == n) {
-            count += 1;
+        if (count >= n) {
+            count = count + (count + 1);
         }
         return count;
     }
 
     public static void main(String[] args) {
-        System.out.println(countCup(7, 6));
+        System.out.println(countCup(213, 6));
     }
 }
