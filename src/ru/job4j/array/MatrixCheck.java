716c290e488e68b56f51da62d0fb24130e24deba
msvolt123 <msvolt123@gmail.com>
2021-06-09 18:43:32 +0300

    Массив из диагонали матрицы.

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -7,8 +7,8 @@ public class MatrixCheck {
             if (board[row][i] != 'X') {
                 result = false;
                 break;
-                }
             }
+        }
         return result;
     }
 
@@ -21,6 +21,14 @@ public class MatrixCheck {
             }
         }
         return result;
+    }
+
+    public static char[] extractDiagonal(char[][] board) {
+        char[] rsl = new char[board.length];
+        for (int i = 0; i < board.length; i++) {
+            rsl[i] = board[i][i];
+        }
+        return rsl;
     }
 }
 
